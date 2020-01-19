672dc942965d22fce887c9fce539308fa14ac8d1
Tyler Gosling <t.gosling61@gmail.com>
2020-01-19 12:17:11 -0500

    second commit

---
+++ FileCleaner/FileCleanerMain.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <Windows.h>
+#include <filesystem>
+
+using namespace std;
+using namespace filesystem;
+
+string getExePath() {
+	char result[MAX_PATH];
+	return string(result, GetModuleFileName(NULL, result, MAX_PATH));
+
+}
+
+int main(int argc, char* argv[]) {
+	int usrInput;
+	string usrPath;
+	string fileType;
+
+	//prompt user
+	cout << "File Cleaner, 2020 (C)" << endl;
+	cout << "What is the directory you would like to search? ";
+	cin << usrPath;
+	cout << "\nWhat type of file would you like to remove?" << endl;
+	cout << "1) .torrent" << endl;
+	cout << "2) .rar" << endl;
+	//get path and files to delete
+	cin >> usrInput;
+	usrPath = getExePath();
+	fileType = ".torrent";
+	cout << "\nYou selected " << usrInput << endl;
+	filesystem::path p = usrPath;
+	cout << "Folder: " << p << endl;
+
+	//iterate through directory to find files
+	for (const auto& dirEntry : recursive_directory_iterator(p)) {
+		//check for files
+		//if its a specified file, delete it
+		if(dirEntry.path().extension() == ".torrent")
+			cout << dirEntry.path().string() << endl;
+	}
+
+}
