53ac09eb14da0917e8f85b06e0ced9f6eee10fa6
Tyler Gosling <t.gosling61@gmail.com>
2020-01-23 21:22:11 -0500

    printMethod and loop user input until terminated

--- FileCleaner/FileCleaner_Main.cpp
+++ FileCleaner/FileCleaner_Main.cpp
@@ -11,13 +11,29 @@ using namespace filesystem;
 string getExePath() {
 	char result[MAX_PATH];
 	return string(result, GetModuleFileName(NULL, result, MAX_PATH));
+}
 
+void printRes(set<string>& s) {
+	int fileSetSize = s.size(), MaxValLngth = s.begin()->size(), count = 0;
+
+	//find maxValLngth
+	for (auto it = s.begin(); it != s.end(); ++it) {
+		if (it->size() > MaxValLngth)
+			MaxValLngth = it->size();
+	}
+
+	for (auto it = s.begin(); it != s.end(); ++it) {
+		if (fileSetSize % 5 == 0)
+			cout << setw(3) << right << ++count << ") " << left << setw(MaxValLngth) << *it << endl;
+		else
+			cout << setw(3) << right << ++count << ") " << left << setw(MaxValLngth) << *it;
+		--fileSetSize;
+	}
 }
 
 int main(int argc, char* argv[]) {
-	string usrPath, usrFile;
+	string usrPath, usrInput;
 	string fileType;
-	map<string, int> fileMap;
 	set<string> fileSet;
 
 	//prompt user
@@ -27,57 +43,45 @@ int main(int argc, char* argv[]) {
 
 	//check directory for file types
 	filesystem::path p = usrPath;
-	int count = 0, torCount = 0;
 	for (const auto& dirEntry : recursive_directory_iterator(p)) {
-		++count;
 			fileSet.insert(dirEntry.path().extension().string());
-			fileMap.insert(pair<string,int>(dirEntry.path().extension().string(), count));
 	}
 
 	//print out directory extensions
-	count = 0;
 	cout << "\nDirectory: " << p << endl;
 	cout << "Directory File Types: " << endl;
-	int fileSetSize = fileSet.size(), MaxValLngth = fileSet.begin()->size();
-
-	for (auto it = fileSet.begin(); it != fileSet.end(); ++it) {
-		if(it->size() > MaxValLngth)
-			MaxValLngth = it->size();
-	}
-
-
-	for (auto it = fileSet.begin(); it != fileSet.end(); ++it) {
-		
-		if (fileSetSize % 5 == 0)
-			cout << setw(3) << right << ++count <<  ") " << left << setw(MaxValLngth) << *it << endl;
-		else
-			cout << setw(3) << right << ++count << ") " << left << setw(MaxValLngth) << *it;
-		--fileSetSize;
-	}
+	
+	printRes(fileSet);
 
 	cout << "\n\nFolder: " << p << endl;
-	cout << "What type of file would you like to remove?" << endl;
 
-	//get path and files to delete
-	cin >> usrFile;
-	fileType = ".torrent";
-	cout << "\nYou selected " << usrFile << endl;
-	
-	
 	int delCount = 0;
-	//iterate through directory to find files
-	for (const auto& dirEntry : recursive_directory_iterator(p)) {
-		//check for files
-		//if its a specified file, delete it
-		if (dirEntry.path().extension() == usrFile) {
-			cout << dirEntry.path().string() << endl;
-			remove(dirEntry);
-			++delCount;
-			if (remove(dirEntry) != 0)
-				cout << "Error deleting file" << endl;
+	while (usrInput != "x") {
+		cout << "\nWhat type of file would you like to remove?" << endl;
+		cout << "\tEnter 'x' to end program, 'p' to print files again." << endl;
+		//get path and files to delete
+		cin >> usrInput;
+		cout << "\nYou selected " << usrInput << endl;
+		//printRes or find files and remove
+		if (usrInput == "p") {
+			printRes(fileSet);
 		}
+		else {
+			//iterate through directory to find files
+			for (const auto& dirEntry : recursive_directory_iterator(p)) {
+				//check for files
+				//if its a specified file, delete it
+				if (dirEntry.path().extension() == usrInput) {
+					cout << dirEntry.path().string() << endl;
+					remove(dirEntry);
+					++delCount;
+					if (remove(dirEntry) != 0)
+						cout << "Error deleting file" << endl;
+				}
+			}
+			cout << "Files deleted: " << delCount << endl;
+		}
+		delCount = 0;
 	}
-
-	cout << "Files deleted: " << delCount << endl;
 
 }
